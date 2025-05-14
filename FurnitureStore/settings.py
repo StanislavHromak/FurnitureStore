008c33e8deff9e03e060115efe4218819e36b3e2
Stanislav_Hromak <gromakstanislav@knu.ua>
2025-05-14 08:43:04 +0300

    add main application, update settings.py, urls.py in FurnitureStore

--- FurnitureStore/settings.py
+++ FurnitureStore/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main.apps.MainConfig',
+    'catalog.apps.CatalogConfig'
 ]
 
 MIDDLEWARE = [
@@ -54,11 +56,11 @@ ROOT_URLCONF = 'FurnitureStore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -112,10 +114,12 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
---
+++ main/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('shop/', views.shop, name='shop'),
+    path('about/', views.about, name='about'),
+    path('design/', views.design, name='design'),
+    path('contact/', views.contact, name='contact'),
+]
